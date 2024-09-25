ee749cba4b4ab02cfa41ba5f5c2366ec678bb020
Daniel de Queiroz <42477879+danieldequeiroz@users.noreply.github.com>
2024-09-24 23:09:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ app.set('view engine', 'ejs'); // Configura o motor de template como EJS
 
 // Defina a rota para exibir os feriados em JSON
 app.get('/api/feriados', (req, res) => {
-  res.json(feriados); // Retorna os feriados como JSON
+  try {
+    res.json(feriados); // Retorna os feriados como JSON
+  } catch (error) {
+    console.error('Erro ao retornar feriados em JSON:', error);
+    res.status(500).json({ message: 'Erro ao retornar feriados em JSON' });
+  }
 });
 
 // Defina a rota para renderizar a view
 app.get('/', (req, res) => {
-  res.render('feriados', { feriados: feriados }); // Renderiza a view feriados.ejs com os feriados
+  try {
+    res.render('feriados', { feriados: feriados }); // Renderiza a view feriados.ejs com os feriados
+  } catch (error) {
+    console.error('Erro ao renderizar view:', error);
+    res.status(500).json({ message: 'Erro ao renderizar view' });
+  }
 });
 
 app.listen(port, () => {
